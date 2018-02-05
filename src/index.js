a235d66b7a3c6c25963d22e83470fd8132e1f473
Arthur236 <arthur.thungu@gmail.com>
2018-02-05 11:59:57 +0300

    [Feature #154594590] Set up redux stuff.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import {Provider} from 'react-redux';
+import {BrowserRouter as Router} from 'react-router-dom';
+import Notifications from 'react-notify-toast';
+import jwt from 'jsonwebtoken';
 import registerServiceWorker from './registerServiceWorker';
+import configureStore from './store/configureStore';
+import routes from './routes';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = configureStore();
+
+ReactDOM.render(
+    <Provider store={store}>
+        <div>
+            <Notifications />
+            <Router>
+                {routes}
+            </Router>
+        </div>
+    </Provider>,
+    document.getElementById('root'));
+
 registerServiceWorker();
